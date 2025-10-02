31db20aa58b0ddf6b795feb7b42ca1fe64ef9fd6
Dylan J. <springtrapkilles@gmail.com>
2025-10-02 09:07:19 -0400

    Enable Reconnect Features on FFmpeg

--- bot.py
+++ bot.py
@@ -775,7 +775,7 @@ async def play_stream(interaction, url):
       return
 
   # Pipe music stream to FFMpeg
-  music_stream = discord.FFmpegPCMAudio(resp, pipe=True, options="-filter:a loudnorm=I=-30:LRA=4:TP=-2")
+  music_stream = discord.FFmpegPCMAudio(resp, pipe=True, options="-filter:a loudnorm=I=-30:LRA=4:TP=-2 -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5")
 
   # Create proper cleanup callback that handles state
   def stream_finished_callback(error):
